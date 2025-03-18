8229e58085d50ea743cedaf78a0942ebfc4d96d6
Zeerunsyou <155188630+Zeerunsyou@users.noreply.github.com>
2025-03-18 18:43:30 -0400

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,43 @@
+const BADGES = [
+  {
+    name: "Subscriber since Jun 29, 2022",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Jun 29, 2022",
+    image: "assets/badges/boost24month.svg",
+  },
+  {
+    name: "Early Supporter",
+    image: "assets/badges/early_supporter.svg",
+  },
+  {
+    name: "Excalicraft Developer",
+    image: "assets/badges/discord_certified_moderator.svg",
+  },
+  {
+    name: "Verified Bot Developer", // Message that appears when you hover over it
+    image: "assets/badges/verified_bot_developer.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+];
+
+const USER_ID = "1234883115810557957"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Streaming", // Discord Status
+    color: "#9441f9",
+  },
+  idle: {
+    name: "Streaming",
+    color: "#9441f9",
+  },
+  online: {
+    name: "Streaming",
+    color: "#9441f9",
+  },
+  offline: {
+    name: "Streaming",
+    color: "#9441f9",
+  },
+};
